b6377477b492b25b839810ec12f26dbf117de02e
jungheejung <heejung.jung@colorado.edu>
2019-11-16 10:05:01 -0500

    participant num 0004 instead of 004

--- task-posnerAR/scripts/step00_experimentDev/s04_posner_AR_counterbalance.py
+++ task-posnerAR/scripts/step00_experimentDev/s04_posner_AR_counterbalance.py
@@ -29,19 +29,19 @@ for sub in list(range(1,151)):
     cue_v = pd.DataFrame()
     invalid = []
     valid = []
-    # 1. load AR generation ________________________________________________________
+# 1. load AR generation ________________________________________________________
     cue_vfilename = os.path.join(dir_posner, 'design', 's01_cue-sequence', 'sub-' + '{:03d}'.format(sub) + '_cuesequence.csv')
     cue_v = pd.read_csv(cue_vfilename, header = None)
     cue_v=cue_v.rename(columns = {0:'AR_invalid_sequence'})
 
-    # 2. count number of valids ________________________________________________________
+# 2. count number of valids ________________________________________________________
     valid_map = {1: 'invalid', 0: 'valid'}
     cue_v['valid_type'] = cue_v.iloc[:,0].map(valid_map)
     cue_v['cue'] = 0
     cue_v['target'] = 0
     a = dict(Counter(cue_v.valid_type))
 
-    # 3 invalid cue left right ________________________________________________________
+# 3 invalid cue left right ________________________________________________________
 
     if (a['invalid']%2) == 0:
         invalid = np.repeat(['left', 'right'], int(a['invalid']//2))
@@ -62,7 +62,7 @@ for sub in list(range(1,151)):
         for ind, loca in enumerate(invalid_loc):
             cue_v.loc[loca, 'cue'] = invalid[ind]
 
-    # 4 valid cue left right ________________________________________________________
+# 4 valid cue left right ________________________________________________________
     if (a['valid']%2) == 0:
         valid = np.repeat(['left', 'right'], int(a['valid']//2))
         np.random.shuffle(valid)
@@ -79,13 +79,13 @@ for sub in list(range(1,151)):
         for ind, loca in enumerate(valid_loc):
             cue_v.loc[loca, 'cue'] = valid[ind]
 
-    # 5 target location ____________________________________________________________
+# 5 target location ____________________________________________________________
     cue_v.loc[(cue_v.valid_type == 'invalid') & (cue_v['cue'] == 'left'), 'target'] = 'right'
     cue_v.loc[(cue_v.valid_type == 'invalid') & (cue_v['cue'] == 'right'), 'target'] = 'left'
     cue_v.loc[(cue_v.valid_type == 'valid') & (cue_v['cue'] == 'left'), 'target'] = 'left'
     cue_v.loc[(cue_v.valid_type == 'valid') & (cue_v['cue'] == 'right'), 'target'] = 'right'
 
-    # 6. load jitter and combine with counterbalance sequence ________________________________________________________
+# 6. load jitter and combine with counterbalance sequence ________________________________________________________
     ind = sub%30
     jitter_filename = os.path.join(dir_posner, 'design', 's03_updatejitter',
         'posner_Events_best_design_of_10000_under_ideal_length_sec_ver-'+str('{0:03d}'.format(ind+1))+'.csv')
@@ -94,5 +94,5 @@ for sub in list(range(1,151)):
     # cue_v['jitter'] = jitter['ISI1']
     cue_v.insert(0, "jitter", jitter['ISI1'], True)
 
-    save_filename = os.path.join(dir_posner, 'design', 's04_counterbalance', 'sub-' + '{:03d}'.format(sub) + '_task-posner_counterbalance.csv')
+    save_filename = os.path.join(dir_posner, 'design', 's04_counterbalance', 'sub-' + '{:04d}'.format(sub) + '_task-posner_counterbalance.csv')
     cue_v.to_csv(save_filename,index=False)
