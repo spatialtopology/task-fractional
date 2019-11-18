ea4a69dd5c833bc16675629a99ca4497c5caf97a
jungheejung <heejung.jung@colorado.edu>
2019-11-18 18:06:32 -0500

    receives ptb parameters, displays old new word ontop of stimuli

--- task-memory/mt_test.m
+++ task-memory/mt_test.m
@@ -1,5 +1,5 @@
-function [cfg,expParam] = mt_test(w,cfg,expParam,logFile,sesName)
-% function [cfg,expParam] = mt_test(w,cfg,expParam,logFile,sesName)
+function [cfg,expParam] = mt_test( p ,cfg,expParam,logFile,sesName)
+% function [cfg,expParam] = mt_test(p.ptb.window,cfg,expParam,logFile,sesName)
 
 % Description:
 %  This function runs the test task. There are no blocks.
@@ -16,8 +16,20 @@ startTime = sprintf('%.2d:%.2d:%.2d',startTime(4),startTime(5),startTime(6));
 expParam.session.(sesName).date = thisDate;
 expParam.session.(sesName).startTime = startTime;
 
+Screen('TextSize', p.ptb.window, cfg.text.basicTextSize);
+Screen('TextFont', p.ptb.window, cfg.text.basicFontName);
+Screen('TextStyle', p.ptb.window, cfg.text.basicFontStyle);
+DrawFormattedText(p.ptb.window, cfg.text.(sesName).instructionsMessage, 'center', 'center', cfg.text.basicTextColor);
+Screen('Flip', p.ptb.window);
+KbStrokeWait;
 % put it in the log file
 fprintf(logFile,'!!! Start of %s (%s) %s %s\n',sesName,mfilename,thisDate,startTime);
+
+%% _______________________ Wait for Trigger to Begin ___________________________
+% DisableKeysForKbCheck([]);
+KbName('UnifyKeyNames');
+KbTriggerWait(p.keys.start);
+KbTriggerWait(p.keys.trigger);
 
 thisGetSecs = GetSecs;
 fprintf(logFile,'%f\t%s\t%s\t%s\n',...
@@ -71,9 +83,9 @@ for s = 1 : length(stimList)
     correctAnswer = oldNew(s);
     if sessionCfg.isFix
         % fixation cross
-        Screen('TextSize', w, cfg.text.basicTextSize);
-        DrawFormattedText(w, cfg.text.fixSymbol, 'center', 'center', cfg.text.basicTextColor);
-        Screen('Flip', w);
+        Screen('TextSize', p.ptb.window, cfg.text.basicTextSize);
+        DrawFormattedText(p.ptb.window, cfg.text.fixSymbol, 'center', 'center', cfg.text.basicTextColor);
+        Screen('Flip', p.ptb.window);
         timeFix = sessionCfg.preStim(1) + ((sessionCfg.preStim(2) - sessionCfg.preStim(1)).*rand(1,1));
         WaitSecs(timeFix);
     end
@@ -83,9 +95,27 @@ for s = 1 : length(stimList)
             stimImgFile(stimImgFile=='\') = '/';
             stimImg = imread(stimImgFile);
             stimImg = uint8(stimImg);
-            imageTexture = Screen('MakeTexture', w, stimImg);
-            Screen('DrawTexture', w, imageTexture, [],[],0,0);
-            [VBLTimestamp StimulusOnsetTime FlipTimestamp] = Screen('Flip', w);
+            imageTexture = Screen('MakeTexture', p.ptb.window, stimImg);
+            Screen('DrawTexture', p.ptb.window, imageTexture, [],[],0,0);
+%                         % 5-2. present scale lines _____________________________________________________
+            Yc = 300; % Y coord
+            cDist = 20; % vertical line depth
+            lXc = -200; % left X coord
+            rXc = 200; % right X coord
+            lineCoords = [lXc lXc lXc rXc rXc rXc; Yc-cDist Yc+cDist Yc Yc Yc-cDist Yc+cDist];
+%             Screen('DrawLines', p.ptb.window, lineCoords,p.fix.lineWidthPix, p.ptb.black);% [p.ptb.xCenter p.ptb.yCenter], 2);
+
+                        % 5-3. present same diff text __________________________________________________
+            textOld = 'old';
+            textNew = 'new';
+            textYc = p.ptb.yCenter + (RectHeight(p.ptb.rect)/2)*.30;
+%             textYc = p.ptb.yCenter + Yc + cDist*4;
+            textRXc = p.ptb.xCenter + rXc; % p.ptb.xCenter+120,
+            textLXc = p.ptb.xCenter - rXc; % p.ptb.xCenter-250-60,
+            DrawFormattedText(p.ptb.window, textOld, textLXc,  textYc, p.ptb.white); % Text output of mouse position draw in the centre of the screen
+            DrawFormattedText(p.ptb.window, textNew, textRXc,  textYc, p.ptb.white); % Text output of mouse position draw in the centre of the screen
+
+            [VBLTimestamp StimulusOnsetTime FlipTimestamp] = Screen('Flip', p.ptb.window);
             
             thisGetSecs = GetSecs;
             fprintf(logFile,'%f\t%s\t%s\t%s\t%s\t%s\n',...
@@ -98,9 +128,9 @@ for s = 1 : length(stimList)
             
         case('w') % show words
             currentWord = stimList{s}(7:end-4);
-            Screen('TextSize', w, cfg.text.basicTextSize);
-            DrawFormattedText(w, currentWord, 'center', 'center', cfg.text.basicTextColor);
-            [VBLTimestamp StimulusOnsetTime FlipTimestamp] = Screen('Flip', w);
+            Screen('TextSize', p.ptb.window, cfg.text.basicTextSize);
+            DrawFormattedText(p.ptb.window, currentWord, 'center', 'center', cfg.text.basicTextColor);
+            [VBLTimestamp StimulusOnsetTime FlipTimestamp] = Screen('Flip', p.ptb.window);
             
             thisGetSecs = GetSecs;
             fprintf(logFile,'%f\t%s\t%s\t%s\t%s\t%s\n',...
@@ -117,17 +147,14 @@ for s = 1 : length(stimList)
 %         keyCode = zeros(1,256);
         timeStim = GetSecs - thisGetSecs;
         while respToBeMade && timeStim < sessionCfg.stim
-            fprintf('wait for response\n');%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % check the keyboard
             [keyIsDown,secs, keyCode] = KbCheck;
-            fprintf('%d\n',keyIsDown);
+%             fprintf('%d\n',keyIsDown);
             if keyCode(KbName(cfg.keys.oldKey))
-                fprintf('OLD KEY\n');%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 respToBeMade = false;
                 answer = '1';
                 RT = secs-StimulusOnsetTime;
             elseif keyCode(KbName(cfg.keys.newKey))
-                fprintf('NEW KEY\n');%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 respToBeMade = false;
                 answer = '0';
                 RT = secs-StimulusOnsetTime;
@@ -143,21 +170,19 @@ for s = 1 : length(stimList)
         
         if respToBeMade && timeStim >= sessionCfg.stim %if not answer made after the presentation time of the stimuli, display a ?
             % Question mark
-            Screen('TextSize', w, cfg.text.basicTextSize);
-            DrawFormattedText(w, cfg.text.respSymbol, 'center', 'center', cfg.text.basicTextColor);
-            Screen('Flip', w);
+            Screen('TextSize', p.ptb.window, cfg.text.basicTextSize);
+            DrawFormattedText(p.ptb.window, cfg.text.respSymbol, 'center', 'center', cfg.text.basicTextColor);
+            Screen('Flip', p.ptb.window);
             
             % Wait for answer
             while respToBeMade
                 % check the keyboard
                 [keyIsDown,secs, keyCode] = KbCheck;
                 if keyCode(KbName(cfg.keys.oldKey))
-                    fprintf('? OLD KEY\n');%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                     respToBeMade = false;
                     answer = '1';
                     RT = secs-StimulusOnsetTime;
                 elseif keyCode(KbName(cfg.keys.newKey))
-                    fprintf('? NEW KEY\n');%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                     respToBeMade = false;
                     answer = '0';
                     RT = secs-StimulusOnsetTime;
@@ -169,13 +194,13 @@ for s = 1 : length(stimList)
         WaitSecs(sessionCfg.stim);
         
         % Question mark
-        Screen('TextSize', w, cfg.text.basicTextSize);
-        DrawFormattedText(w, cfg.text.respSymbol, 'center', 'center', cfg.text.basicTextColor);
-        Screen('Flip', w);
+        Screen('TextSize', p.ptb.window, cfg.text.basicTextSize);
+        DrawFormattedText(p.ptb.window, cfg.text.respSymbol, 'center', 'center', cfg.text.basicTextColor);
+        Screen('Flip', p.ptb.window);
         
         % Wait for answer
         respToBeMade = true;
-        keyCode = zeros(1,256);
+%         keyCode = zeros(1,256);
         while respToBeMade
             % check the keyboard
             [keyIsDown,secs, keyCode] = KbCheck;
@@ -202,8 +227,8 @@ for s = 1 : length(stimList)
         RT);
 
     % isi
-    Screen('FillRect', w, cfg.screen.bgColor);
-    Screen('Flip', w);
+    Screen('FillRect', p.ptb.window, cfg.screen.bgColor);
+    Screen('Flip', p.ptb.window);
     WaitSecs(sessionCfg.isi);
 end
 
